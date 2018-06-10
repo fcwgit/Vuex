e4f249a0b46bd3ced0403b6655e5b6cb398c27d6
fcwgit <fcwgit@github.com>
2018-06-10 21:04:42 +0800

    vuex

---
+++ src/components/Count.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <hr/>
+        <!-- <h3>{{$store.state.count}}</h3> -->
+        <head>{{$store.state.count}}</head>
+    </div>
+    
+</template>
+
+<script>
+    import store from '../vuex/store';
+    // export default {
+    //     data(){
+    //         return{
+    //             msg:'Hello  Vuex '
+    //         }
+    //     },
+    //     store
+    // }
+
+    export default {
+        name: 'Count',
+        data () {
+            return {
+                msg: 'Hello Vuex'
+            }
+        },
+        store
+    }
+</script>
---
+++ src/vuex/store.js
@@ -0,0 +1,15 @@
+import Vue from 'Vue';
+import Vuex from 'Vuex';
+Vue.use(Vuex);
+
+const state={
+    count:1
+}
+
+// export default new Vuex.store({
+//     state
+// })
+
+export default new Vuex.Store({
+    state
+})
